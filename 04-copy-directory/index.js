271b5a2a83790405c43684e61dca7682524f2c30
Kirsawka <83959481+Kirsawka@users.noreply.github.com>
2022-05-20 16:37:25 +0300

    feat: add 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const path = require('path');
+const fs = require('fs');
 
+fs.readdir(path.join(__dirname, 'files-copy'), (error, files) => {
+  if (error) {
+    fs.mkdir(path.join(__dirname, 'files-copy'), (error) => {
+      if (error) return console.error(error.message);
+    });
+    copyDir();
+  } else {
+    for (const file of files) {
+      fs.unlink(path.join(__dirname, 'files-copy', file), error => {
+        if (error) return console.error(error.message);
+      });
+    }
+    copyDir();
+  }
+});
+
+const copyDir = () => {
+  fs.readdir(path.join(__dirname, 'files'), (error, files) => {
+    if (error) return console.error(error.message);
+    for (const file of files) {
+      fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), error => {
+        if (error) return console.error(error.message);
+      });
+    }
+  });
+};
